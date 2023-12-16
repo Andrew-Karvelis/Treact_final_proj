102ab12d410694fc1aa7783dfddc27b236254e55
Andrew-Karvelis <akarvelis.work@gmail.com>
2023-12-16 14:03:51 +1100

    Media queries complete, +Animation to burger menu

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// HAMBURGER NAV MENU
+
 function openMenu() {
     document.body.classList += " menu--open"
 }
@@ -5,3 +7,19 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove("menu--open")
 }
+
+// ACCORDION NOT WORKING - @https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_accordion_animate
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + "px";
+//     }
+//   });
+// }
